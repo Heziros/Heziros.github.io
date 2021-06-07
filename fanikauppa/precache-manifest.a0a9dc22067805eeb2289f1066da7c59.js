63d3f80dc5035f7388532de73a18f62031f3cba8
Derizion <jani.kesola@gmail.com>
2021-06-07 11:34:27 +0300

    Fanikauppa link

---
+++ fanikauppa/precache-manifest.a0a9dc22067805eeb2289f1066da7c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32f14a8012f67396314e26fb702f607",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "f3ce1585b9d9661fa70f",
+    "url": "/fanikauppa/static/css/main.737ce89f.chunk.css"
+  },
+  {
+    "revision": "d119745731e880b35a56",
+    "url": "/fanikauppa/static/js/2.59d5e86e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.59d5e86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ce1585b9d9661fa70f",
+    "url": "/fanikauppa/static/js/main.30348c74.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
